445256b11e4c15ccbf4ac9a622b355d16205fddc
Nitcheu <ph.favrelle@gmail.com>
2021-11-21 15:13:57 +0100

    Version finale

--- ChiffreEnLettre/ChiffreEnLettre.c
+++ ChiffreEnLettre/ChiffreEnLettre.c
@@ -9,11 +9,6 @@ int main()
 	double nombre;
 	char* mots;
 
-	for (int i = 0; i < 100; i++) {
-		mots = traitementNombre(i);
-		afficherLettres(mots);
-	}
-
 	nombre = saisirNombre();
 	mots = traitementNombre(nombre);
 	afficherLettres(mots);
